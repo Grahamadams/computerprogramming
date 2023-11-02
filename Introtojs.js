e08cf8fddc6b00fd8a8a5d50fb824220018cf70f
Grahamadams <144175331+Grahamadams@users.noreply.github.com>
2023-11-02 14:55:25 -0600

    Create Introtojs.js

---
+++ Introtojs.js
@@ -0,0 +1,36 @@
+const myName = "Graham"
+
+let favColor = "blue"
+
+favColor = "red"
+
+console.log {favColor}
+
+let age = 30
+
+mainperson = {
+    name: "Graham",
+    age: 30,
+    favColor: "blue"
+    favFoods = ["steak", "ice cream", "potatoes"]
+}
+
+favthings = [2, "movies"]
+
+const isAlive = true
+
+function describeperson(person) {
+    alert("MISSING PERSON ALERT: " + person.name)
+    console. log(`MISSING PERSON ALERT`)
+}
+
+describeperson(mainperson) 
+
+if (age > 30 && age < 50) {
+    alert("You're on your way to middle age.")
+} else if (age < 30) {
+    alert("You're a baby.")
+} else {
+    ('How are you alive') }
+
+    document .getElementById("title").innerHTML = "hello world"
